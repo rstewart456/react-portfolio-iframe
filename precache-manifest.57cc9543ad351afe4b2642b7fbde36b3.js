f20a623f2de960e4414e934bdf9e196d67fa7077
rstewart456 <rstewart456@gmail.com>
2020-04-21 13:09:57 -0400

    Updates

---
+++ precache-manifest.57cc9543ad351afe4b2642b7fbde36b3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f83b93180367368586234b1a1e9c8462",
+    "url": "/rportfolio/index.html"
+  },
+  {
+    "revision": "260ca60dee6437cfb1dc",
+    "url": "/rportfolio/static/css/main.050ba301.chunk.css"
+  },
+  {
+    "revision": "5cf7a94276b83b729b01",
+    "url": "/rportfolio/static/js/2.cda3d528.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/rportfolio/static/js/2.cda3d528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "260ca60dee6437cfb1dc",
+    "url": "/rportfolio/static/js/main.db8ade3c.chunk.js"
+  },
+  {
+    "revision": "452f78fd49b5837e57b9",
+    "url": "/rportfolio/static/js/runtime-main.4c9ac32a.js"
+  },
+  {
+    "revision": "efd27daa42960cffe48c582c2fbab45a",
+    "url": "/rportfolio/static/media/aman.efd27daa.jpg"
+  },
+  {
+    "revision": "1cb86b023f301261e84d96de70496c95",
+    "url": "/rportfolio/static/media/coding-bg.1cb86b02.png"
+  },
+  {
+    "revision": "42a5ba6fd30776ac9be74afc00ce82da",
+    "url": "/rportfolio/static/media/spinner.42a5ba6f.gif"
+  }
+]);
